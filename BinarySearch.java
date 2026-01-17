416391213cfd44ff713ea71ee7771724c7665c68
AnasSiddiqui2 <muhammadanassiddiqui252@gmail.com>
2026-01-17 17:24:20 +0500

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,25 @@
+public class BinarySearch {
+
+    public static int binarySearchIterative(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) return mid;
+            else if (target > arr[mid])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9};
+        int target = 7;
+        int index = binarySearchIterative(arr, target);
+        System.out.println("Target found at index: " + index);
+    }
+}
