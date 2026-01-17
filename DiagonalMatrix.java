694b154e4d78c5f7b1a2c8719493fb5616d2e6ad
AnasSiddiqui2 <muhammadanassiddiqui252@gmail.com>
2026-01-17 17:16:56 +0500

    Create DiagonalMatrix.java

---
+++ DiagonalMatrix.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DiagonalMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] mat = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                mat[i][j] = sc.nextInt();
+
+        System.out.print("Main Diagonal: ");
+        for (int i = 0; i < rows && i < cols; i++) {
+            System.out.print(mat[i][i] + " ");
+        }
+
+        sc.close();
+    }
+}
